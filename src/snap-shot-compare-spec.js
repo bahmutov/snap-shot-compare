9f3ced0bf1f7b71844fb6e19c688e9be14c9944a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-08-14 17:23:31 -0400

    initial

---
+++ src/snap-shot-compare-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* eslint-env mocha */
+const snapShotCompare = require('.')
+
+describe('snap-shot-compare', () => {
+  it('write this test', () => {
+    console.assert(snapShotCompare, 'should export something')
+  })
+})
