5f33a35ae5eb2eaeba912f423c4f0a309eca25e3
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-08-14 17:28:18 -0400

    initial code

--- src/snap-shot-compare-spec.js
+++ src/snap-shot-compare-spec.js
@@ -1,11 +1,13 @@
 'use strict'
 
+const la = require('lazy-ass')
+const is = require('check-more-types')
 
 /* eslint-env mocha */
 const snapShotCompare = require('.')
 
 describe('snap-shot-compare', () => {
-  it('write this test', () => {
-    console.assert(snapShotCompare, 'should export something')
+  it('is a function', () => {
+    la(is.fn(snapShotCompare))
   })
 })
