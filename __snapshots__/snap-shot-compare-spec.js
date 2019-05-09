35f11a2e2513b536eb2c144210deb20d2ec84858
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-05-09 10:04:10 -0400

    chore: upgrade to use snap-shot-it v7

--- __snapshots__/snap-shot-compare-spec.js
+++ __snapshots__/snap-shot-compare-spec.js
@@ -1,7 +1,9 @@
-exports['snap-shot-compare works for different objects 1'] = `{
+exports['snap-shot-compare works for different objects 1'] = `
+{
 - foo: "foo"
 + bar: "bar"
-}`
+}
+`
 
 exports['snap-shot-compare works for different objects with json option 1'] = {
   "message": "{\n- foo: \"foo\"\n+ bar: \"bar\"\n}",
@@ -13,15 +15,18 @@ exports['snap-shot-compare works for different objects with json option 1'] = {
   }
 }
 
-exports['snap-shot-compare works for different objects with noColor option 1'] = `{
+exports['snap-shot-compare works for different objects with noColor option 1'] = `
+{
 - foo: "foo"
 + bar: "bar"
-}`
+}
+`
 
-exports['snap-shot-compare works for text 1'] = ` line 1
+exports['snap-shot-compare works for text 1'] = `
+ line 1
 -line 2
 -line 3
 +line 2 changed
 +third line is new
-`
 
+`
--- __snapshots__/utils-spec.js
+++ __snapshots__/utils-spec.js
@@ -1,16 +1,27 @@
-exports['disparity compareText diffs multiple text 1'] = ` line 1
+exports['disparity compareText diffs multiple text 1'] = `
+ line 1
 [31m-line 2[39m
 [32m+line 2b[39m
  line 3
+
 `
 
-exports['disparity compareText returns diff 1'] = ` line 1
+exports['disparity compareText returns diff 1'] = `
+ line 1
 -line 2
 +line 2b
  line 3
+
 `
 
+exports['disparity compareText returns JSON diff 1'] = {
+  "message": " line 1\n-line 2\n+line 2b\n line 3\n",
+  "expected": "line 1\nline 2\nline 3",
+  "value": "line 1\nline 2b\nline 3"
+}
+
 exports['disparity compareLongText diffs long text 1'] = `
+
 ----------
 Difference
 ----------
@@ -33,17 +44,11 @@ line 3
 --------
 Diff end
 --------
-`
 
-exports['disparity compareText returns JSON diff 1'] = {
-  "message": " line 1\n-line 2\n+line 2b\n line 3\n",
-  "expected": "line 1\nline 2\nline 3",
-  "value": "line 1\nline 2b\nline 3"
-}
+`
 
 exports['disparity compareLongText returns JSON diff 1'] = {
   "message": " line 1\n-line 2\n+line 2b\n line 3\n",
   "expected": "line 1\nline 2\nline 3",
   "value": "line 1\nline 2b\nline 3"
 }
-
